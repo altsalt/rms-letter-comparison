5f97dfd539f99c5b780df11f279aefa2b4761b70
Wm Salt Hale <salt@altsalt.net>
2021-03-26 18:44:07 -0700

    lots of print statements, except block is not catching the exception and then everything breaks...

--- code/00-gather_data.py
+++ code/00-gather_data.py
@@ -14,6 +14,7 @@ np.random.seed(42)
 
 from github import Github
 from github import RateLimitExceededException
+from github import GithubException
 
 token = os.getenv('GITHUB_TOKEN')
 gh = Github(token)
@@ -30,58 +31,124 @@ rr_users = rr.get_contributors(anon=1)
 sr_users = sr.get_contributors(anon=1)
 
 def log_writer(text, filename):
+  print(' >< in log_writer')
   file = open(filename + '.log', 'a')
   file.write(str(text) + '\n')
   file.close()
 
 def rate_limiter(call, location):
+  print(' <> in rate_limiter')
   successful_call = False
-  while not successful_call:
+  while(not successful_call):
     try:
+      print(' <> trying to call')
+      print(str(successful_call))
+#      print(str(call))
       response = call
+#      print(str(response))
       successful_call = True
+      if(successful_call):
+        print('successful_call')
     except RateLimitExceededException as e:
+      print(' <> trying to write 1.1')
+      print(' <> ' + str(location))
       log_writer(location, 'rate')
+      print(' <> trying to write 1.2')
+      print(' <> ' + '[' + time.asctime() + '] Rate limit at time of error:')
       log_writer('[' + time.asctime() + '] Rate limit at time of error:', 'rate')
+      print(' <> trying to write 1.3')
+      print(' <> ' + str(gh.get_rate_limit().core))
       log_writer(gh.get_rate_limit().core, 'rate')
+      print(' <> trying to write 1.4')
       log_writer('---', 'rate')
       time.sleep(10)
       continue
-    except Exception as e:
-      print('[' + time.asctime() + '] An unhandled error occurred:')
+    except GithubException as e:
+      print(' <> trying to write 2.1')
+      print('[' + time.asctime() + '] An unhandled GH error occurred:')
+      print(' <> trying to write 2.2')
       print(e)
+      print(' <> trying to write 2.3')
       print('---')
       log_writer(location, 'error')
       log_writer('An unhandled error occurred:', 'error')
       log_writer(e, 'error')
       log_writer('---', 'error')
+      time.sleep(10)
       continue
+    except Exception as e:
+      print(' <> trying to write 3.1')
+      print('[' + time.asctime() + '] An unhandled error occurred:')
+      print(' <> trying to write 3.2')
+      print(e)
+      print(' <> trying to write 3.3')
+      print('---')
+      log_writer(location, 'error')
+      log_writer('An unhandled error occurred:', 'error')
+      log_writer(e, 'error')
+      log_writer('---', 'error')
+      time.sleep(10)
+      continue
+  print('out of try block')
+  print(str(response))
   return(response)
 
 def get_user_info(user, repo):
-  utype = user.type
+  print(' . in get_user_info')
+  uname = rate_limiter(user.name, '(get user name)')
+  uemail = rate_limiter(user.email, '(get user email)')
+  utype = rate_limiter(user.type, '(get user type)')
+  print(utype)
   if(utype == 'Anonymous'):
-    if(user.email != ''):
-      queried_users = rate_limiter(gh.search_users(user.email), '(email check)')
-    elif(user.name != ''):
-      queried_users = rate_limiter(gh.search_users(user.name + ' in:name'), '(name check)')
-    else:
-      queried_users = None
-      
+    print(' > is anonymous')
+    print(uname)
+    print(uemail)
+    print(utype)
+    queried_users = None
+    if(uemail != ''):
+      print(' > email check')
+      queried_users = rate_limiter(gh.search_users(uemail), '(email check)')
+      print(queried_users)
+      print(' > count check 1')
+      qucount = rate_limiter(queried_users.totalCount, '(user count)')
+      print(qucount)
+      if((qucount == 0) and (uname != '')):
+        print(' >> name check in email')
+        queried_users = rate_limiter(gh.search_users(uname + ' in:name'), '(name check)')
+        print(queried_users)
+        print(' > count check 2')
+        qucount = rate_limiter(queried_users.totalCount, '(user count)')
+        print(qucount)
+      print(' > in email passed zero and name')
+    elif(uname != ''):
+      print(' > name check')
+      queried_users = rate_limiter(gh.search_users(uname + ' in:name'), '(name check)')
+      print(queried_users)
+      print(' > count check 3')
+      qucount = rate_limiter(queried_users.totalCount, '(user count)')
+      print(qucount)
     if(queried_users == None):
       utype = 'Anonymous'
     else:
+      print(' > count check 4')
       qucount = rate_limiter(queried_users.totalCount, '(user count)')
+      print(qucount)
       if(qucount == 0):
         utype = 'Anonymous'
       elif(qucount > 1):
         utype = 'Multiple'
       else:
+        print(' > user id check')
         user = rate_limiter(queried_users.get_page(0)[0], '(get individual user)')
+        print(user)
         utype = 'Queried'
-        
+  print(' > passed anonymous')
+  print(uname)
+  print(uemail)
+  print(utype)
   if((utype == 'Anonymous') or (utype == 'Multiple')):
-    info = {
+    print(' >> store unidentified')
+    info = rate_limiter({
       'ID': None,
       'NodeID': None,
       'Username': None,
@@ -128,9 +195,10 @@ def get_user_info(user, repo):
       'ReceivedEventsURL': None,
       'SubscriptionsURL': None,
       'Repo': repo
-    }
+    }, '(store anonymous user)')
   else:
-    info = {
+    print(' >> store identified')
+    info = rate_limiter({
       'ID': user.id,
       'NodeID': user.node_id,
       'Username': user.login,
@@ -177,7 +245,7 @@ def get_user_info(user, repo):
       'ReceivedEventsURL': user.received_events_url,
       'SubscriptionsURL': user.subscriptions_url,
       'Repo': repo
-    }
+    }, '(store identified user)')
   return(info)
 
 
